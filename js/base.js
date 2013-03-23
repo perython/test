1d1253b289d38d8f7f99cc97a71076eefc102988
perython <alexander.bubeev@gmail.com>
2013-03-23 12:01:27 +0900

    starting creating simple game

--- js/base.js
+++ js/base.js
@@ -7,15 +7,57 @@ function get_random_color() {
   return color;
 }
 
-$(function(){
-  var s = '';
-  var cell_num = 50;
-  for (var i=0; i<cell_num; i++) {
-  	s += '<div class="cell" style="background: '+get_random_color()+'""></div>';
-  }	
-  $('#grid').append(s);
+function shuffle(o){
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
 
+$(function(){
+  var cell_pairs_num = 25;
+  var s = '';
+  var colors = new Array();
+  for (var i=0; i<cell_pairs_num*2; i++) {
+  	s += '<div class="cell"></div>';
+  	if (i < cell_pairs_num) {
+  	  var color = get_random_color();
+  	  colors.push(color);
+  	  colors.push(color);
+  	}
+  }
+  shuffle(colors);
+
+  $('#grid').append(s).find('.cell').each(function(index, element){
+  	$(this).css('background', colors[index])
+  });
+  
+  var clicked = false;
+  var first;
+  $('#grid').click(function(e){
+  	if (clicked) {
+      compare(first, $(e.target));
+  	} else {
+  	  clicked = true;
+  	  first = $(e.target);
+  	  first.addClass('chosen');
+  	}
+  })
+
+  function compare(first, second) {
+  	var equal = false;
+  	if (first.css('background') == second.css('background')) equal = true;
+  	second.addClass('chosen');
+  	if (equal) {
+  	  first.css('background', '#fff');
+  	  second.css('background', '#fff');
+  	  $('#info .points').text(parseInt($('#info .points').text()) + 1);
+  	}
+  	clicked = false;
+  	first.removeClass('chosen');
+  	second.removeClass('chosen');
+  }
+  /*
   $('.cell').mouseenter(function(){
   	$(this).css('background', get_random_color());
   });
+  //*/
 });
